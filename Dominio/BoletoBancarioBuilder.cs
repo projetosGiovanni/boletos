1ba4ed71afa9335c4d228c1c1de9a2d16750853d
Giovanni Nicoletti <giovanni.nic@gmail.com>
2017-07-04 02:25:07 -0300

    criando builder

---
+++ Dominio/BoletoBancarioBuilder.cs
@@ -0,0 +1,90 @@
+﻿using BoletoNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsBoletos.Dominio
+{
+    class BoletoBancarioBuilder
+    {
+        public short CodigoBanco { get; private set; }
+        public bool MostrarCodigoCarteira { get; private set; }
+        public Boleto Boleto { get; private set; }
+
+        public BoletoBancarioBuilder ParaCodigoBanco(short codigoBanco)
+        {
+            this.CodigoBanco = codigoBanco;
+            return this;
+        }
+
+        public BoletoBancarioBuilder ComBoleto(Boleto boleto)
+        {
+            this.Boleto = boleto;
+            return this;
+        }
+
+        public BoletoBancarioBuilder ComMostrarCodigoCarteira(bool mostrarCodigoCarteira)
+        {
+            this.MostrarCodigoCarteira = mostrarCodigoCarteira;
+            return this;
+        }
+
+        public void GerarBoletoPdf()
+        {
+            BoletoBancario objBoletoBancario = NewBoletoBancario();
+
+            GerarPdf(objBoletoBancario);
+        }
+
+        public void GerarBoletoHtml()
+        {
+            BoletoBancario objBoletoBancario = NewBoletoBancario();
+
+            GerarHtml(objBoletoBancario);
+        }
+
+        private BoletoBancario NewBoletoBancario()
+        {
+            BoletoBancario objBoletoBancario = new BoletoBancario()
+            {
+                CodigoBanco = this.CodigoBanco,
+                Boleto = this.Boleto,
+                MostrarCodigoCarteira = this.MostrarCodigoCarteira
+            };
+            //validar boleto
+            objBoletoBancario.Boleto.Valida();
+            return objBoletoBancario;
+        }
+
+        private void GerarPdf(BoletoBancario _bolBB)
+        {
+            //Crio array com os dados
+            byte[] Pdf_Buffer = _bolBB.MontaBytesPDF(true);
+
+            //Crio o arquivo em disco e um fluxo, sobrescrevendo o arquivo anterior, se existir.
+            FileStream Stream = new FileStream(@"boleto.pdf", FileMode.Create);
+
+            //Escrevo arquivo no fluxo
+            Stream.Write(Pdf_Buffer, 0, Pdf_Buffer.Length);
+
+            //Fecho fluxo pra finalmente salvar em disco
+            Stream.Close();
+
+            //Abrir PDF gerado
+            System.Diagnostics.Process.Start(@"boleto.pdf");
+        }
+
+        private void GerarHtml(BoletoBancario _bolBB)
+        {
+            //crio o boleto como um arquivo *.html
+            _bolBB.MontaHtmlNoArquivoLocal("boleto.html");
+
+            //Abrir HTML no navegador padrão
+            System.Diagnostics.Process.Start(@"boleto.html");
+        }
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows.Forms;
 using BoletoNet;
+using WinFormsBoletos.Dominio;
 
 namespace WinFormsBoletos
 {
@@ -14,70 +15,68 @@ namespace WinFormsBoletos
 
         private void btnHTML_Click(object sender, EventArgs e)
         {
-            //crio o boleto como um arquivo *.html
-            RetornaBoletoPreenchido().MontaHtmlNoArquivoLocal("boleto.html");
-
-            //Abrir HTML no navegador padrão
-            System.Diagnostics.Process.Start(@"boleto.html");
+            new BoletoBancarioBuilder()
+                .ParaCodigoBanco(1)
+                .ComBoleto( RetornaBoletoPreenchido() )
+                .ComMostrarCodigoCarteira(true)
+                .GerarBoletoHtml();
         }
 
         private void btnPDF_Click(object sender, EventArgs e)
         {
-            //Crio array com os dados
-            byte[] Pdf_Buffer = RetornaBoletoPreenchido().MontaBytesPDF(true);
-
-            //Crio o arquivo em disco e um fluxo, sobrescrevendo o arquivo anterior, se existir.
-            FileStream Stream = new FileStream(@"boleto.pdf", FileMode.Create);
-
-            //Escrevo arquivo no fluxo
-            Stream.Write(Pdf_Buffer, 0, Pdf_Buffer.Length);
-
-            //Fecho fluxo pra finalmente salvar em disco
-            Stream.Close();
-
-            //Abrir PDF gerado
-            System.Diagnostics.Process.Start(@"boleto.pdf");
+            new BoletoBancarioBuilder()
+                .ParaCodigoBanco(1)
+                .ComBoleto( RetornaBoletoPreenchido() )
+                .ComMostrarCodigoCarteira(true)
+                .GerarBoletoPdf();
         }
 
-        private BoletoBancario RetornaBoletoPreenchido() {
+        private Boleto RetornaBoletoPreenchido() {
 
-            DateTime vencimento = Convert.ToDateTime("04/07/2017"); //txtVencimento.Text; 
-            decimal valorBoleto = Convert.ToDecimal("1,59"); // txtValor.Text; 
-            string numeroDocumento = "501953"; //"00000174"; //"10028528";  //"498261";
-            int cedente_convenio = 2815189; //1220950;
-            string boleto_nossoNumero = "8757"; //"0" + cedente_convenio; //000[1002852] [2815189] [0001002852] nosso numero valido
+            DateTime vencimento = Convert.ToDateTime("04/07/2017"); 
+            decimal valorBoleto = Convert.ToDecimal("19,24");
+            string numeroDocumento = "501953"; 
+            int cedente_convenio = 2815189; 
+            string boleto_nossoNumero = "8757"; 
 
             #region informações pessoais do sacado / pagador         
             string sacado_cpfCnpj = "034.648.884-28";
-            string sacado_nome = "GIOVANNI DE PAIVA NICOLETTI"; //txtSacado.Text;
+            string sacado_nome = "GIOVANNI DE PAIVA NICOLETTI"; 
 
-            Endereco objEndereco = new Endereco();
-            objEndereco.End = "Rua Rodolfo Garcia, 1988";
-            objEndereco.Bairro = "Lagoa Nova";
-            objEndereco.CEP = "59064370";
-            objEndereco.Cidade = "Natal";
-            objEndereco.UF = "RN";
+            Endereco objEndereco = new Endereco()
+            {
+                End = "Rua Rodolfo Garcia, 1988",
+                Bairro = "Lagoa Nova",
+                CEP = "59064370",
+                Cidade = "Natal",
+                UF = "RN"
+            };
             #endregion
 
-            #region objCedente - criando o objeto cedente, o agente que vai receber o dinheiro
-            Cedente objCedente = new Cedente("08.286.940/0001-09", "PROCURADORIA GERAL DO ESTADO-RN", "3795", "8", "00005011", "3");
-            objCedente.Codigo = cedente_convenio.ToString();
-            objCedente.Carteira = "18";
-            objCedente.Convenio = cedente_convenio;            
+            #region criando o cedente, o agente que vai receber o dinheiro
+            Cedente objCedente = new Cedente("08.286.940/0001-09", "PROCURADORIA GERAL DO ESTADO-RN", "3795", "8", "00005011", "3")
+            {
+                Codigo = cedente_convenio.ToString(),
+                Carteira = "18",
+                Convenio = cedente_convenio
+            };
             #endregion
 
-            #region objBoleto - criando um boleto que tem um sacado e especie de documento
-            Boleto objBoleto = new Boleto(vencimento, valorBoleto, objCedente.Carteira, boleto_nossoNumero, objCedente);
-            objBoleto.NumeroDocumento = numeroDocumento;
-            objBoleto.LocalPagamento = "PAGÁVEL NA REDE BANCARIA ATÉ O VENCIMENTO";
-            objBoleto.Sacado = new Sacado(sacado_cpfCnpj, sacado_nome, objEndereco);
-            objBoleto.EspecieDocumento = new EspecieDocumento_BancoBrasil("23");
+            #region criando um boleto que tem um sacado e especie de documento
+            Boleto objBoleto = new Boleto(vencimento, valorBoleto, objCedente.Carteira, boleto_nossoNumero, objCedente)
+            {
+                ValorCobrado = valorBoleto,
+                NumeroDocumento = numeroDocumento,
+                LocalPagamento = "PAGÁVEL NA REDE BANCARIA ATÉ O VENCIMENTO",
+                Sacado = new Sacado(sacado_cpfCnpj, sacado_nome, objEndereco),
+                EspecieDocumento = new EspecieDocumento_BancoBrasil("23")
+            };
             #endregion
 
-            #region objBoleto - Adicionando uma Instrucao_BancoBrasil texto decritivo em HTML 
+            #region Adicionando as Instruções no Boleto 
             Instrucao_BancoBrasil instrucao = new Instrucao_BancoBrasil(1);
-            instrucao.Descricao += "<small>";
-            instrucao.Descricao = "Não conceder qualquer desconto ou abatimento";
+            instrucao.Descricao = "<small>";
+            instrucao.Descricao += "Não conceder qualquer desconto ou abatimento";
             instrucao.Descricao += "<br>";
             instrucao.Descricao += "Não receber/pagar após o vencimento, pois o pagamento em atraso está sujeito ";
             instrucao.Descricao += "a juros de mora, multa de mora de 0,13% ao dia até o limite de 4% ";
@@ -86,23 +85,13 @@ namespace WinFormsBoletos
             instrucao.Descricao += "Após o vencimento, comparecer à sede da PGE ou núcleos regionais ou ainda imprimir novo ";
             instrucao.Descricao += "boleto pelo site da PGE (www.pge.rn.gov.br).";
             instrucao.Descricao += "<br>";
-            instrucao.Descricao += "Vlr. Devido R$ x,xx + Vlr. Honorários R$ 0,00 = R$ " + valorBoleto;
+            instrucao.Descricao += "Vlr. Total do Boleto R$ " + valorBoleto;
             instrucao.Descricao += "</small>";
             objBoleto.Instrucoes.Add(instrucao);
             #endregion
 
-            #region objBoletoBancario - cria o boleto a ser impresso
-            BoletoBancario objBoletoBancario = new BoletoBancario();
-            objBoletoBancario.CodigoBanco = 1;
-            objBoletoBancario.Boleto = objBoleto;
-            //objBoletoBancario.Boleto.Carteira = objCedente.Carteira;
-            //objBoletoBancario.Boleto.LocalPagamento = "PAGÁVEL NA REDE BANCARIA ATÉ O VENCIMENTO";
-            objBoletoBancario.Boleto.ValorCobrado = valorBoleto;
-            objBoletoBancario.MostrarCodigoCarteira = true;
-            objBoletoBancario.Boleto.Valida();
-            #endregion
+            return objBoleto;
+        }                 
 
-            return objBoletoBancario;
-        }
     }
 }
